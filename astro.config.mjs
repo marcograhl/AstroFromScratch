bbc76b9126becada91683a89301a1eded5bc469e
marcograhl <manograhl@gmail.com>
2022-01-19 17:33:43 +0000

    Test diff. Port

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
 	vite: {
 		server: {
 			hmr: {
-				clientPort: process.env.HMR_HOST ? 443: 3000,
+				clientPort: process.env.HMR_HOST ? 443: 24678,
 				host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
 			}
 		}
