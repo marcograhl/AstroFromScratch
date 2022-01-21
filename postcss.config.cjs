2021d78216594a292821d02afb263b39ae0e8f8d
marcograhl <manograhl@gmail.com>
2022-01-21 17:02:57 +0000

    feat: Setup PostCSS and Open Props
    - added PostCSS Config
    - added nesting and import Plugins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,9 @@
 const postcssNesting = require('postcss-nesting')
-
+const postcssImport = require('postcss-import')
 
 module.exports = {
     plugins: [
+        postcssImport({ path: ['src'] }),
         postcssNesting(),
     ]
 }
